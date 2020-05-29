9c4310c28809918f82e9579b49374a20506b34f0
unchi <unchitarou>
2020-05-29 09:42:14 +0900

    create views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+#.はカレントディレクトリー、ここではblog.のかわりかな
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #{"name":value}
+    return render(request, 'blog/post_list.html', {'posts': posts})
