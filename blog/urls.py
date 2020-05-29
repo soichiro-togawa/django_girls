72932d43e5e860e6d57b969a6576c9c763f00562
unchi <unchitarou>
2020-05-29 11:19:16 +0900

    iroirotuika

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 #url、view、urlの名前(好きなもの)、の順番
 urlpatterns = [
+    #記載されたviewをurlと紐づける
     path('', views.post_list, name='post_list'),
+    #post/整数のurl
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
